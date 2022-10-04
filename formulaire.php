8d2cc795df44b56026405384332873cda2bfae77
CaptainMarty <romslavoiz88@gmail.com>
2022-10-04 10:48:15 +0200

    first-comit

---
+++ formulaire.php
@@ -0,0 +1,37 @@
+<?php
+
+print_r($_POST);
+
+if(
+    isset($_POST['name']) && !empty($_POST['name']) &&
+    isset($_POST['email']) && !empty($_POST['email']) &&
+    isset($_POST['subject']) && !empty($_POST['subject']) &&
+    isset($_POST['message']) && !empty($_POST['message'])
+){
+
+    /* Variable pour les mails */
+
+    $name = $_POST['name'];
+    $to = $_POST['email'];
+    $subject = $_POST['subject'];
+    $body = '
+                <p>Nom : '. $_POST['name'] . '</p>
+                <p>email : '. $_POST['email'] . '</p>
+                <p>subject : '. $_POST['subject'] . '</p>
+                <p>message : '. $_POST['message'] . '</p>
+    ';
+ 
+    $headers = "From: Romain";  
+    $headers .= "Reply-To: romslavoiz88@gmail.com\r\n";  
+    $headers .= "Return-Path: romslavoiz88@gmail.com\r\n";  
+    $headers .= "X-Mailer: PHP5\n";  
+    $headers .= 'MIME-Version: 1.0' . "\n";  
+    $headers .= 'Content-type: text/html; charset=iso-8859-1' . "\r\n";  
+
+    /* condition envoie mail */
+    
+    if ( mail($to,$subject,$body,$headers) ) {
+        $ok = 'Votre mail a été envoyé avec succès';
+    }  
+
+}
---
+++ index.php
@@ -0,0 +1,475 @@
+<!DOCTYPE html>
+<html lang="fr">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+  <link rel="stylesheet" href="./css/bootstrap.css">
+  <link rel="stylesheet" href="./script/bootstrap-icons/bootstrap-icons.css">
+  <link rel="stylesheet" href="./script/boxicons/css/boxicons.min.css">
+  <link rel="stylesheet" href="./script/glightbox/css/glightbox.min.css">
+  <link rel="stylesheet" href="./script/remixicon/remixicon.css">
+  <link rel="stylesheet" href="./script/swiper/swiper-bundle.min.css">
+
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.0/css/all.min.css" />
+
+  <link rel="stylesheet" href="stylev2.css">
+
+  <title>CV Lavoisier Romain</title>
+</head>
+
+<body>
+
+  <header id="header">
+    <div class="container">
+      <h1><a href="index.html"></a><strong>Lavoisier Romain</strong></h1>
+      <h2>Je suis <span><strong>Développeur Web et Streamer.</strong></span></h2>
+
+      <nav id="navbar" class="navbar">
+        <ul>
+          <li><a href="#header" class="nav-link">Accueil</a></li>
+          <li><a href="#moi" class="nav-link">Moi</a></li>
+          <li><a href="#experience" class="nav-link">Expériences</a></li>
+          <li><a href="#contact" class="nav-link">Contact</a></li>
+        </ul>
+        <i class="bi bi-list mobile-nav-toggle"></i>
+      </nav>
+
+      <div class="social-links">
+        <a href="https://www.facebook.com/profile.php?id=100086251924777" class="facebook" target="_blank"><i class="fab fa-facebook"></i></a>
+        <a href="https://www.instagram.com/captain_martyy_/" class="instagram" target="_blank"><i class="fab fa-instagram"></i></a>
+        <a href="https://www.linkedin.com/in/romain-lavoisier-a166b1250/" target="_blank" class="linkedin"><i class="fab fa-linkedin"></i></a>
+        <a href="https://www.twitch.tv/captain_marty_" target="_blank" class="twitch"><i class="fab fa-twitch"></i></a>
+        <a href="https://www.youtube.com/channel/UCkp0qEiQUS81bxl754Vwq4w" target="_blank" class="youtube"><i class="fab fa-youtube"></i></a>
+      </div>
+
+    </div>
+  </header>
+
+  <!-- Section "moi" -->
+  <section id="moi" class="moi">
+    <div class="container">
+      <div class="section-titre">
+        <h2>Moi</h2>
+        <p>En apprendre plus sur moi</p>
+      </div>
+      <div class="row">
+        <div class="col-lg-4" data-aos="fade-right">
+          <img src="photo_cv.png" alt="photo de moi" class="img-fluid">
+        </div>
+        <div class="col-lg-8 pt-4 pt-lg-0 content" data-aos="fade-left">
+          <h3>UI &  JS</h3>
+          <p class="fst-italic">J'ai appris ces langages pendant une session de formation durant 10 mois.
+          </p>
+          <div class="row">
+            <div class="col-lg-6">
+              <ul>
+                <li>
+                  <i class="fas fa-chevron-right"></i>
+                  <p><strong>Anniversaire:</strong> 20 Août 1992</p>
+                </li>
+                <li><i class="fas fa-chevron-right"></i>
+                  <p><strong>Twitch:</strong>Captain_Marty_</p>
+                </li>
+                <li><i class="fas fa-chevron-right"></i>
+                  <p><strong>Téléphone:</strong>07 69 71 74 80</p>
+                </li>
+                <li><i class="fas fa-chevron-right"></i>
+                  <p><strong>Ville:</strong>Epinal, Vosges</p>
+                </li>
+              </ul>
+            </div>
+            <div class="col-lg-6">
+              <ul>
+                <li><i class="fas fa-chevron-right"></i>
+                  <p><strong>Âge:</strong>30</p>
+                </li>
+                <li><i class="fas fa-chevron-right"></i>
+                  <p><strong>Niveau:</strong>Bac+2</p>
+                </li>
+                <li><i class="fas fa-chevron-right"></i>
+                  <p><strong>Mail:</strong>romslavoiz88@gmail.com</p>
+                </li>
+                <li><i class="fas fa-chevron-right"></i>
+                  <p><strong>Freelance:</strong>Disponible</p>
+                </li>
+              </ul>
+            </div>
+          </div>
+          <p>Depuis 2020, je consacre mon temps au streaming sur twitch en présentant des émissions différentes allant
+            de la musique
+            aux jeux vidéos, et cette année je me suis lancé dans le développement web afin de proposer mes services de
+            codeur
+            aux futurs streamers en créant un outil personnalisé, simple, facile et complet.
+          </p>
+        </div>
+      </div>
+      <!-- Section compteur -->
+
+      <div class="counts">
+        <div class="row">
+          <div class="col-lg-3 col-md-6">
+            <div class="count-box">
+              <i class="bi bi-twitch"></i>
+              <span class="purecounter" data-purecounter-start="0" data-purecounter-end="678" data-purecounter-duration="1.5"></span>
+              <p>Twitch Followers</p>
+            </div>
+          </div>
+
+          <div class="col-lg-3 col-md-6">
+            <div class="count-box">
+              <i class="bi bi-youtube"></i>
+              <span class="purecounter" data-purecounter-start="0" data-purecounter-end="255" data-purecounter-duration="1.5"></span>
+              <p>Youtube Followers</p>
+            </div>
+          </div>
+
+          <div class="col-lg-3 col-md-6">
+            <div class="count-box">
+              <i class="bi bi-briefcase"></i>
+              <span class="purecounter" data-purecounter-start="0" data-purecounter-end="6" data-purecounter-duration="1.5"></span>
+              <p>Github Project</p>
+            </div>
+          </div>
+
+          <div class="col-lg-3 col-md-6">
+            <div class="count-box">
+              <i class="bi bi-book"></i>
+              <span class="purecounter" data-purecounter-start="0" data-purecounter-end="4" data-purecounter-duration="1.5"></span>
+              <p>Diplômes</p>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div class="skills">
+        <div class="section-titre">
+          <h2>Compétences</h2>
+        </div>
+        <div class="row skills-content">
+          <div class="col-lg-6">
+            <div class="progress">
+              <span class="skill">HTML<i class="val">80%</i></span>
+              <div class="progress-bar" role="progressbar" aria-valuenow="80" aria-valuemin="0" aria-valuemax="100">
+              </div>
+            </div>
+            <div class="progress">
+              <span class="skill">CSS<i class="val">80%</i></span>
+              <div class="progress-bar" role="progressbar" aria-valuenow="80" aria-valuemin="0" aria-valuemax="100">
+              </div>
+            </div>
+
+            <div class="progress">
+              <span class="skill">JAVASCRIPT<i class="val">35%</i></span>
+              <div class="progress-bar" role="progressbar" aria-valuenow="35" aria-valuemin="0" aria-valuemax="100">
+              </div>
+            </div>
+          </div>
+
+          <div class="col-lg-6">
+            <div class="progress">
+              <span class="skill">BOOTSTRAP<i class="val">60%</i></span>
+              <div class="progress-bar" role="progressbar" aria-valuenow="60" aria-valuemin="0" aria-valuemax="100">
+              </div>
+            </div>
+            <div class="progress">
+              <span class="skill">SUITE ADOBE<i class="val">90%</i></span>
+              <div class="progress-bar" role="progressbar" aria-valuenow="90" aria-valuemin="0" aria-valuemax="100">
+              </div>
+            </div>
+
+            <div class="progress">
+              <span class="skill">SUITE GOOGLE<i class="val">75%</i></span>
+              <div class="progress-bar" role="progressbar" aria-valuenow="75" aria-valuemin="0" aria-valuemax="100">
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+
+
+      <!-- Interêts -->
+
+      <div class="interests">
+        <div class="section-titre">
+          <h2>Interêts</h2>
+        </div>
+        <div class="row">
+          <div class="col-lg-3 col-md-4">
+            <div class="icon-box">
+              <i class="ri-film-line" style="color:cadetblue"></i>
+              <h3>Cinéma</h3>
+            </div>
+          </div>
+          <div class="col-lg-3 col-md-4 mt-4 mt-md-0">
+            <div class="icon-box">
+              <i class="ri-gamepad-line" style="color:blue"></i>
+              <h3>Jeux Vidéos</h3>
+            </div>
+          </div>
+          <div class="col-lg-3 col-md-4 mt-4 mt-md-0">
+            <div class="icon-box">
+              <i class="ri-twitch-line" style="color:#A970FF"></i>
+              <h3>Streaming</h3>
+            </div>
+          </div>
+          <div class="col-lg-3 col-md-4 mt-4 mt-lg-0">
+            <div class="icon-box">
+              <i class="ri-pencil-fill" style="color:brown"></i>
+              <h3>Dessin</h3>
+            </div>
+          </div>
+          <div class="col-lg-3 col-md-4 mt-4">
+            <div class="icon-box">
+              <i class="ri-knife-fill" style="color:rgb(42, 138, 165)"></i>
+              <h3>Cuisine</h3>
+            </div>
+          </div>
+          <div class="col-lg-3 col-md-4 mt-4">
+            <div class="icon-box">
+              <i class="ri-star-line" style="color:rgb(90, 6, 70)"></i>
+              <h3>Astronomie</h3>
+            </div>
+          </div>
+          <div class="col-lg-3 col-md-4 mt-4">
+            <div class="icon-box">
+              <i class="ri-exchange-dollar-line" style="color:rgb(55, 161, 39)"></i>
+              <h3>Cryptomonnaie</h3>
+            </div>
+          </div>
+          <div class="col-lg-3 col-md-4 mt-4">
+            <div class="icon-box">
+              <i class="ri-run-line" style="color:rgb(140, 165, 42)"></i>
+              <h3>Course</h3>
+            </div>
+          </div>
+          <div class="col-lg-3 col-md-4 mt-4">
+            <div class="icon-box">
+              <i class="ri-suitcase-2-line" style="color:rgb(180, 11, 157)"></i>
+              <h3>Voyage</h3>
+            </div>
+          </div>
+          <div class="col-lg-3 col-md-4 mt-4">
+            <div class="icon-box">
+              <i class="ri-camera-3-fill" style="color:rgb(213, 213, 213)"></i>
+              <h3>Photographie</h3>
+            </div>
+          </div>
+          <div class="col-lg-3 col-md-4 mt-4">
+            <div class="icon-box">
+              <i class="ri-bear-smile-line" style="color:rgb(63, 33, 33)"></i>
+              <h3>Animaux</h3>
+            </div>
+          </div>
+          <div class="col-lg-3 col-md-4 mt-4">
+            <div class="icon-box">
+              <i class="ri-question-answer-line" style="color:rgb(42, 48, 165)"></i>
+              <h3>Réseaux Sociaux</h3>
+            </div>
+          </div>
+
+        </div>
+      </div>
+    </div>
+  </section>
+
+  <section id="experience" class="experience">
+    <div class="container">
+
+      <div class="section-titre">
+        <h2>Etudes</h2>
+        <p> Expériences et Etudes</p>
+      </div>
+      <div class="row">
+        <div class="col-lg-6">
+          <h3 class="experience-titre">Etudes</h3>
+          <div class="experience-item">
+            <h4>D.W.W.M (en cours)</h4>
+            <h5>2022 - 2023</h5>
+            <p><em>Voulant faire une reconversion professionnelle dans le milieu informatique, j'ai voulu ajouter des
+                compétences de Web design à mes services de Community Manager indépendant.</em>
+            </p>
+          </div>
+          <div class="experience-item">
+            <h4>B.A.F.A</h4>
+            <h5>2010 - 2013</h5>
+            <p><em>Ayant passé mon enfance en colonie de vacances et centre aéré, j'ai décidé de le faire à mon tour en
+                obtenant ce diplôme.</em>
+            </p>
+          </div>
+          <div class="experience-item">
+            <h4>Plombier - Chauffagiste</h4>
+            <h5>2007 - 2012</h5>
+            <p><em>Il faut bien commencer quelque part. J'ai donc passé 5 ans de ma jeune vie à excercer ce métier en
+                tant qu'apprenti.</em>
+            </p>
+          </div>
+          <div class="experience-item">
+            <h4>jeunes sapeur pompier</h4>
+            <h5>2011</h5>
+            <p><em>Après 3 ans de formation "Jeune Sapeur Pompier" dans le Centre de Secours de Bruyères (Vosges) j'ai
+                été reçu en tant que Pompier Volontaire par le SDIS de Epinal.</em>
+            </p>
+          </div>
+        </div>
+        <div class="col-lg-6">
+          <h3 class="experience-titre">Expériences Professionnelles</h3>
+          <div class="experience-item">
+            <h4>Community Manager</h4>
+            <h5>Octobre 2021</h5>
+            <p><em>Studio de jeux vidéos "SUPERCUBE"</em>
+            </p>
+            <p>
+            <ul>
+              <li>Benchmark de réseaux sociaux</li>
+              <li>Refonte complet du serveur Discord</li>
+              <li>Création de jeu et post sur les réseaux sociaux</li>
+            </ul>
+            </p>
+          </div>
+          <div class="experience-item">
+            <h4>Community Manager</h4>
+            <h5>Septembre 2021</h5>
+            <p><em>Radio "VOSGES FM"</em>
+            </p>
+            <p>
+            <ul>
+              <li>Programmation de Publications sur les réseaux sociaux</li>
+              <li>Photographe et monteur photo</li>
+              <li>Modération et réponse des commentaires</li>
+            </ul>
+            </p>
+          </div>
+        </div>
+
+      </div>
+    </div>
+  </section>
+
+  <section id="contact" class="contact">
+    <div class="container">
+      <div class="section-titre">
+        <h2>Contact</h2>
+        <p>Contactez-moi</p>
+      </div>
+
+      <div class="row mt-2">
+        <div class="col-md-6 d-flex align-items-stretch">
+          <div class="info-box">
+            <i class="bx bx-map"></i>
+            <h3>Mon Adresse</h3>
+            <p>10 rue Simone et jean Lurçat, 88600, Bruyères (Vosges)</p>
+          </div>
+        </div>
+
+        <div class="col-md-6 mt-4 mt-md-0 d-flex align-items-stretch">
+          <div class="info-box">
+            <i class="bx bx-share-alt"></i>
+            <h3>Réseaux Sociaux</h3>
+            <div class="social-links">
+              <a href="https://www.facebook.com/profile.php?id=100086251924777" target="_blank" class="facebook"><i class="bi bi-facebook"></i></a>
+              <a href="https://www.instagram.com/captain_martyy_/" target="_blank" class="instagram"><i class="bi bi-instagram"></i></a>
+              <a href="https://www.linkedin.com/in/romain-lavoisier-a166b1250/" target="_blank" class="linkedin"><i class="bi bi-linkedin"></i></a>
+              <a href="https://www.twitch.tv/captain_marty_" target="_blank" class="twitch"><i class="bi bi-twitch"></i></a>
+              <a href="https://www.youtube.com/channel/UCkp0qEiQUS81bxl754Vwq4w" target="_blank" class="youtube"><i class="bi bi-youtube"></i></a>
+
+            </div>
+          </div>
+        </div>
+
+        <div class="col-md-6 mt-4 d-flex align-items-stretch">
+          <div class="info-box">
+            <i class="bx bx-envelope"></i>
+            <h3>Mon Email</h3>
+            <p>
+              <SCRIPT TYPE="text/javascript">
+                document.write('romslavoiz88@' + 'gmail.com')
+              </SCRIPT>
+
+            </p>
+          </div>
+        </div>
+
+        <div class="col-md-6 mt-4 d-flex align-items-stretch">
+          <div class="info-box">
+            <i class="bx bx-phone-call"></i>
+            <h3>Appelez-moi</h3>
+            <p>+33 07 69 71 74 80</p>
+          </div>
+        </div>
+
+      </div>
+
+      <?php
+        if(isset($ok)) {
+          echo $ok;
+        }
+      ?>
+
+      <form action="formulaire.php" method="post" role="form" class="php-email-form mt-4">
+        <div class="row">
+          <div class="col-md-6 form-group">
+            <input type="text" name="name" class="form-control" id="name" placeholder="Votre Nom" required>
+          </div>
+          <div class="col-md-6 form-group mt-3 mt-md-0">
+            <input type="email" class="form-control" name="email" id="email" placeholder="Votre Email" required>
+          </div>
+        </div>
+        <div class="form-group mt-3">
+          <input type="text" class="form-control" name="subject" id="subject" placeholder="Objet" required>
+        </div>
+        <div class="form-group mt-3">
+          <textarea class="form-control" name="message" rows="5" placeholder="Message" required></textarea>
+        </div>
+        <div class=" text-center mt-3">
+          <button type="submit">Envoyer</button></div>
+      </form>
+    </div>
+
+
+  </section>
+
+
+  <div class="credits">
+    Site conçu par Lavoisier Romain.
+    <button type="button" class="btn btn-info" data-bs-toggle="modal" data-bs-target="#exampleModal">
+      <i class="ri-door-closed-line"></i>
+    </button>
+    <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+      <div class="modal-dialog">
+        <div class="modal-content">
+          <div class="modal-header">
+            <h5 class="modal-title" id="exampleModalLabel">Easter Egg</h5>
+            <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+          </div>
+          <div class="modal-body">
+            <iframe width="460" height="332" src="https://www.youtube.com/embed/GBIIQ0kP15E"
+              title="YouTube video player" frameborder="0"
+              allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+              allowfullscreen></iframe>
+          </div>
+          <div class="modal-footer">
+            <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Retour</button>
+          </div>
+        </div>
+      </div>
+    </div>
+
+  </div>
+
+
+  <script src="./script/purecounter/purecounter_vanilla.js"></script>
+  <script src="./js/bootstrap.bundle.min.js"></script>
+  <script src="./script/glightbox/js/glightbox.min.js"></script>
+  <script src="./script/isotope-layout/isotope.pkgd.min.js"></script>
+  <script src="./node_modules/swiper/swiper-bundle.min.js"></script>
+  <script src="./script/waypoints/noframework.waypoints.js"></script>
+  <script src="./js/main.js"></script>
+
+
+
+
+</body>
+
+</html>
